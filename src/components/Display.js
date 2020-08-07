5b4352538c62455fa8880ad835bbb5bb0461af53
Shubham Raj <shubham14p3@gmail.com>
2020-08-08 02:07:48 +0530

    added Display component

---
+++ src/components/Display.js
@@ -0,0 +1,14 @@
+/* eslint-disable react/prefer-stateless-function */
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class Button extends React.Component {
+  render() {
+    const { name } = this.props;
+    return <button type="submit">{name}</button>;
+  }
+}
+
+Button.propTypes = {
+  name: PropTypes.string.isRequired,
+};
