06eccf46445b203d8e5968864f8d22695cb2938b
Shubham Raj <shubham14p3@gmail.com>
2020-08-09 23:23:16 +0530

    modified button panel components

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,45 +1,40 @@
-/* eslint-disable class-methods-use-this */
-import React from 'react';
-import Button from './Button';
+import React from "react";
+import Button from "./Button";
 
-export default class ButtonPanel extends React.Component {
-  render() {
-    return (
+function ButtonPanel() {
+  return (
+    <div>
       <div>
-        <div>
-          {this.renderButton('AC')}
-          {this.renderButton('+/-')}
-          {this.renderButton('%')}
-          {this.renderButton('÷')}
-        </div>
-        <div>
-          {this.renderButton('7')}
-          {this.renderButton('8')}
-          {this.renderButton('9')}
-          {this.renderButton('X')}
-        </div>
-        <div>
-          {this.renderButton('4')}
-          {this.renderButton('5')}
-          {this.renderButton('6')}
-          {this.renderButton('-')}
-        </div>
-        <div>
-          {this.renderButton('1')}
-          {this.renderButton('2')}
-          {this.renderButton('3')}
-          {this.renderButton('+')}
-        </div>
-        <div>
-          {this.renderButton('0')}
-          {this.renderButton('.')}
-          {this.renderButton('=')}
-        </div>
+        <Button name="AC" />
+        <Button name="+/-" />
+        <Button name="%" />
+        <Button name="÷" />
       </div>
-    );
-  }
-
-  renderButton(name) {
-    return <Button name={name} />;
-  }
+      <div>
+        <Button name="7" />
+        <Button name="8" />
+        <Button name="9" />
+        <Button name="X" />
+      </div>
+      <div>
+        <Button name="4" />
+        <Button name="5" />
+        <Button name="6" />
+        <Button name="-" />
+      </div>
+      <div>
+        <Button name="1" />
+        <Button name="2" />
+        <Button name="2" />
+        <Button name="+" />
+      </div>
+      <div>
+        <Button name="0" />
+        <Button name="." />
+        <Button name="=" />
+      </div>
+    </div>
+  );
 }
+
+export default ButtonPanel;
