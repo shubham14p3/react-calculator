74a0f0100544765d4ebfd0e2e434b5c357c6e3c8
Shubham Raj <shubham14p3@gmail.com>
2020-08-11 00:04:37 +0530

    added main index js  with event

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './style/index.scss';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import App from "./components/App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById("root"));
