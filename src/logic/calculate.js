79a97482ff107c45d782ca948e8d46432675e821
Shubham Raj <shubham14p3@gmail.com>
2020-08-12 01:48:36 +0530

    remodified Calculate logic

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,70 +1,88 @@
-import Operate from './operate';
+import isNum from '../utils/isNum';
+import operate from './operate';
 
-const Calculate = (calculator, buttonName) => {
-  const { total, next, operation } = calculator;
-
-  if (['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'].includes(buttonName)) {
-    if (operation) {
-      return { total, next: next ? next + buttonName : buttonName, operation };
-    }
-    return { total: total ? total + buttonName : buttonName, next, operation };
-  }
-
-  if (buttonName === 'AC') {
-    return { total: null, next: null, operation: null };
-  }
-  if (buttonName === '+/-') {
-    if (next) {
-      return {
-        total,
-        next: Operate(next, '-1', 'X'),
-        operation,
-      };
-    }
+const calculate = (dataObject, buttonName) => {
+  if(buttonName === 'AC') {
     return {
-      total: Operate(total, '-1', 'X'),
-      next,
-      operation,
-    };
-  }
-  if (buttonName === '.') {
-    if (next) {
-      return { total, next: next + buttonName, operation };
-    }
-    if (operation) {
-      return { total, next: `0${buttonName}`, operation };
-    }
-    return { total: total + buttonName, next, operation };
-  }
-  if (buttonName === '=') {
-    if (operation === '÷' && next === '0') {
-      return {
-        total: 'No division by 0',
-        next: null,
-        operation: null,
-      };
-    }
-    return {
-      total: next ? Operate(total, next, operation) : total,
-      next: null,
       operation: null,
+      total: null,
+      next: null
+    }
+  }
+
+  if (buttonName === '+/-') {
+    if (dataObject.next) {
+      return { next: (-1 * parseFloat(dataObject.next)).toString() };
+    }
+    if (dataObject.total) {
+      return { total: (-1 * parseFloat(dataObject.total)).toString() };
+    }
+  }
+
+  if (isNum(buttonName)) {
+    if (buttonName === '0' && dataObject.next === '0') {
+      return {total: null, next:null, operation: null};
+    }
+    if (dataObject.operation) {
+      if (dataObject.next) {
+        return { next: dataObject.next + buttonName };
+      }
+      return { next: buttonName };
+    }
+    if (dataObject.next) {
+      return {
+        next: dataObject.next + buttonName,
+        total: null,
+      };
+    }
+
+    return {
+      next: buttonName,
+      total: null,
     };
   }
-  if (operation) {
-    if (operation === '÷' && next === '0') {
+  
+
+  if (buttonName === '=') {
+    if (dataObject.next && dataObject.operation) {
       return {
-        total: 'No division by 0',
+        total: operate(dataObject.total, dataObject.next, dataObject.operation),
         next: null,
         operation: null,
       };
     }
+    return {};
+  }
+
+  if (buttonName === '.') {
+    if (dataObject.next) {
+      return { next: dataObject.next + '.' };
+    }
+    
+    if (dataObject.total) {
+      return { total: dataObject.total + '.' };
+    }
+    return { total: '0.' };
+  }
+
+  if (dataObject.operation) {
     return {
-      total: Operate(total, next, operation),
+      total: operate(dataObject.total, dataObject.next, dataObject.operation),
       next: null,
       operation: buttonName,
     };
   }
-  return { total, next: null, operation: buttonName };
-};
 
-export default Calculate;
+  if (dataObject.total && !dataObject.next) {
+    return { operation: buttonName };
+  }
+
+
+  return {
+    total: dataObject.next,
+    next: null,
+    operation: buttonName,
+  };
+}
+
+export default calculate;
